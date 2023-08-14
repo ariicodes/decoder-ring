6401d334255dfb619e974f31775973192af38fef
Ariela <arielaisrael12@gmail.com>
2023-08-13 22:02:15 -0400

    algorithm complete along with descriptive comments

--- src/substitution.js
+++ src/substitution.js
@@ -4,15 +4,47 @@
 // of the anonymous function on line 6
 
 const substitutionModule = (function () {
-  // you can add any code you want within this function scope
+	// you can add any code you want within this function scope
 
-  function substitution(input, alphabet, encode = true) {
-    // your solution code here
-  }
+	const toArray = str => str.split('');
 
-  return {
-    substitution,
-  };
+	function substitution(input, alphabet, encode = true) {
+		// error handing 👇
+		try {
+			if (!alphabet || alphabet.length !== 26 || /(.).*\1/.test(alphabet)) {
+				return false;
+			}
+		} catch {
+			return false;
+		}
+
+    // variable declarations 👇
+		let result = '';
+		const inputArr = toArray(input.toLowerCase());
+		const standardAlphaArr = toArray('abcdefghijklmnopqrstuvwxyz');
+		const subAlphaArr = toArray(alphabet);
+
+		inputArr.forEach(char => {
+      // checking whether a character in the input is a space 👇
+			if (char === ' ') {
+				result += ' ';
+      // encoding 👇
+			} else if (encode === true) {
+				const index = standardAlphaArr.indexOf(char);
+				result += subAlphaArr[index];
+      // decoding 👇
+			} else {
+        const index = subAlphaArr.indexOf(char);
+				result += standardAlphaArr[index];
+      }
+		});
+
+		return result;
+	}
+
+	return {
+		substitution,
+	};
 })();
 
 module.exports = { substitution: substitutionModule.substitution };
